0af4d375d86b11e026a69fca8458a984a642bf48
Suareza1992 <suareza1992@gmail.com>
2024-12-10 14:13:44 -0400

    Task 1.

---
+++ csharp-hello_world/1-build_new_project.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Check if folder already exists.
+
+if [ -d  "1-new_project" ]; then
+	echo "Folder '1-new_project' already exists."
+	exit 1
+fi
+
+# Create a new C# project and build it.
+dotnet new console -o 1-new_project && dotnet build 1-new_project
