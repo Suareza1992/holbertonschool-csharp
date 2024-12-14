4b4049d8dd5bd4b18f21979b3bea644919293193
Suareza1992 <suareza1992@gmail.com>
2024-12-14 16:04:06 -0400

    Task 9.

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		CultureInfo culture = new CultureInfo("en-US");
+		Console.WriteLine(string.Format("Percent: {0:P2} \nCurrency: {1:C2}\n", percent, currency));
+	}
+}
