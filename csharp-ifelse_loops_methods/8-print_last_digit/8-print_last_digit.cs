1bbc5d418a2b86d16a13007a314ec99ac11ba62f
Suareza1992 <suareza1992@gmail.com>
2024-12-16 01:01:00 -0400

    Task 8 with main file.

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        // Calculate the last digit using the modulus operator
+        int lastDigit = number % 10;
+
+        // Convert to positive if the last digit is negative
+        if (lastDigit < 0)
+            lastDigit = -lastDigit;
+
+        // Print the last digit
+        Console.Write(lastDigit);
+
+        // Return the last digit
+        return lastDigit;
+    }
+}
