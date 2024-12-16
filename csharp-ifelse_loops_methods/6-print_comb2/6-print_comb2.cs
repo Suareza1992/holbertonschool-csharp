c48bffd7015940c30b156a2c0ee310b584761716
Suareza1992 <suareza1992@gmail.com>
2024-12-16 00:50:17 -0400

    Task 6.

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int first = 0; first < 10; first++)
+        {
+            for (int second = first + 1; second < 10; second++)
+            {
+                if (first != 0 || second != 1)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write($"{first}{second}");
+            }
+        }
+        Console.WriteLine();
+    }
+}
