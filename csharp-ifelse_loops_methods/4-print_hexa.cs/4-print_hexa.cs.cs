a937bdca4c4378847fcc1b551f68560470fb6237
Suareza1992 <suareza1992@gmail.com>
2024-12-15 23:45:46 -0400

    Task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa.cs/4-print_hexa.cs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number <= 98; number++)
+        {
+            Console.WriteLine($"{number} = 0x{number:x}");        }
+    }
+}
