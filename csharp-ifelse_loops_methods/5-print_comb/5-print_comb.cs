cff3cc3d36322c20096a1a67b33f92b1c0d5fbc1
Suareza1992 <suareza1992@gmail.com>
2024-12-16 00:46:16 -0400

    Task 4 & 5 directory name correction.

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number < 100; number++)
+        {
+            if (number < 99)
+            {
+                System.Console.Write($"{number:D2}, ");
+            }
+            else
+            {
+                System.Console.Write($"{number:D2}\n");
+            
+            }
+        }
+        
+    }
+}
