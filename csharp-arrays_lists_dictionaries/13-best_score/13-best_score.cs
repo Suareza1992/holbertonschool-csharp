5dc9c64d5af4985f287d3377aba4be2a2826e8bc
Suareza1992 <suareza1992@gmail.com>
2024-12-19 03:17:48 -0400

    Task 13.

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int i = 0;
+        string j = "";
+
+        if (myList.Count == 0)
+            return "None";
+        foreach (KeyValuePair<string, int> x in myList)
+        {
+            if (x.Value >= i)
+            {
+                i = x.Value;
+                j = x.Key;
+            }
+        }
+
+        return j;
+    }
+}
