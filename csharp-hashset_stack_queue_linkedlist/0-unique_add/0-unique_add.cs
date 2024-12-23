6d29369bb49ca9604119459d1dc46640d3d75f83
Suareza1992 <suareza1992@gmail.com>
2024-12-23 03:03:54 -0400

    Task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var newList = new List<int>();
+        var sum = 0;
+
+        foreach (var number in myList)
+        {
+            if (!newList.Contains(number))
+                newList.Add(number);
+        }
+
+        foreach (var number in newList)
+            sum += number;
+
+        return sum;
+    }
+}
