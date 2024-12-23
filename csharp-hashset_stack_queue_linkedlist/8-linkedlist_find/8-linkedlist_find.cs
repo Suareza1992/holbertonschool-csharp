357b61b667df267c9fe7891101fe5de1e2783681
Suareza1992 <suareza1992@gmail.com>
2024-12-23 04:19:12 -0400

    Task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int node = 0;
+        if(myLList.Contains(value))
+        {
+            foreach (int i in myLList)
+            {
+                if (i == value)
+                    return node;
+                node++;
+            }
+        }
+        else
+        {
+            return (-1);
+        }
+
+        return -1;
+    }
+}
