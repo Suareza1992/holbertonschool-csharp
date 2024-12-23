6697246613f73b121614af652b553a913c940675
Suareza1992 <suareza1992@gmail.com>
2024-12-23 04:31:44 -0400

    Task 12

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count == 0)
+        {
+            return myLList.AddFirst(n);
+        }
+
+        LinkedListNode<int> current = myLList.First;
+        while (current != null)
+        {
+            if (current.Value >= n)
+            {
+                return myLList.AddBefore(current, n);
+            }
+            current = current.Next;
+        }
+
+        
+        return myLList.AddLast(n);
+    }
+}
