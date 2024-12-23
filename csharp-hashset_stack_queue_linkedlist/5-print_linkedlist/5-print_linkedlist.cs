54998ea1e8788e307cdc89a6f388b96b6ca10da2
Suareza1992 <suareza1992@gmail.com>
2024-12-23 04:01:59 -0400

    Task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            LinkedList<int> listEmpty = new LinkedList<int>();
+            return listEmpty;
+        }
+
+        List<int> nLList = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            nLList.Add(i);
+            Console.WriteLine(i);
+        }
+
+        LinkedList<int> nLList1 = new LinkedList<int>(nLList);
+        return nLList1;
+    }
+}
