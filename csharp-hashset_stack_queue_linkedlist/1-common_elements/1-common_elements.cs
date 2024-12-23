73fd2c3738ab7b86534e6cfbffeb7945b4963f23
Suareza1992 <suareza1992@gmail.com>
2024-12-23 03:12:08 -0400

    Task 1.

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> List1, List<int> List2)
+    {
+        var newList = new List<int>();
+        foreach (int i in List1)
+        {
+            foreach (int j in List2)
+            {
+                if (i == j)
+                    newList.Add(i);
+            }
+
+        }
+        return newList;
+    }
+}
