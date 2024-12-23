9f0f244c3758a229055163b6b2505d4c2ac95a60
Suareza1992 <suareza1992@gmail.com>
2024-12-23 04:28:21 -0400

    Task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int index = 0;
+        foreach (int i in myLList)
+            index += i;
+        return index;
+    }
+}
