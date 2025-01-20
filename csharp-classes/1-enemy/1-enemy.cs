b6ea4d7a7af30bc933fa39a99c4f690a57ef4aeb
Suareza1992 <suareza1992@gmail.com>
2025-01-20 01:23:19 -0400

    Task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> public class zombie</summary>
+    class Zombie
+    {
+        /// <summary> field health</summary>
+        public int health;
+        /// public constructor.
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
