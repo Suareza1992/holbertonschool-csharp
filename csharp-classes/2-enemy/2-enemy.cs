034e5b3458927e7d0e76bdac6a50125d0bfaa9cc
Suareza1992 <suareza1992@gmail.com>
2025-01-20 01:26:34 -0400

    Task 2

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> public class zombie</summary>
+    class Zombie
+    {
+        /// <summary> field health</summary>
+        public int health;
+        /// public constructor.
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+    }
+}
