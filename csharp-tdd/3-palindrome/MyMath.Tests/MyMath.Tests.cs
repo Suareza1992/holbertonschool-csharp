52619da6fe694bae751356b27c627d78c5ba437f
Suareza1992 <suareza1992@gmail.com>
2025-01-29 01:07:39 -0400

    Task 3 cs changes

--- csharp-tdd/3-palindrome/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/3-palindrome/MyMath.Tests/MyMath.Tests.cs
@@ -1,15 +1,58 @@
-﻿namespace MyMath.Tests;
+﻿using NUnit.Framework;
 
-public class Tests
+namespace Text.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class StrTests
     {
-    }
+        [TestCase("racecar")]
+        [TestCase("LEVEL")]
+        [TestCase("daD")]
+        [TestCase("RaCeCAr")]
+        public void IsPalindrome_WhenStringIsPalindrome_ReturnsTrue(string testString)
+        {
+            // Act
+            bool isPalindrome = Str.IsPalindrome(testString);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            // Assert
+            Assert.That(isPalindrome == true);
+        }
+
+        [TestCase("ra ce ca     r")]
+        [TestCase("L:EV EL...")]
+        [TestCase("da,,,,D")]
+        [TestCase("R aC   eC:::::,,Ar")]
+        public void IsPalindrome_WhenStringIsPalindromeWithSpaceAndOrPunctuation_ReturnsTrue(string testString)
+        {
+            // Act
+            bool isPalindrome = Str.IsPalindrome(testString);
+
+            // Assert
+            Assert.That(isPalindrome == true);
+        }
+
+        [Test]
+        public void IsPalindrome_WhenStringIsEmpty_ReturnsTrue()
+        {
+            // Arrange - init empty string
+            var testString = string.Empty;
+
+            // Act
+            bool isPalindrome = Str.IsPalindrome(testString);
+
+            // Assert
+            Assert.That(isPalindrome == true);
+        }
+
+        [TestCase("Mother")]
+        [TestCase("brother")]
+        [TestCase("FATHER")]
+        public void IsPalindrome_WhenStringIsNotPalindrome_ReturnsFalse(string testString)
+        {
+            // Act
+            bool isPalindrome = Str.IsPalindrome(testString);
+
+            // Assert
+            Assert.That(isPalindrome == false);
+        }
     }
 }
